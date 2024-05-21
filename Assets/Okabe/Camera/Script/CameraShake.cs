4ee3412b45c6b16b1afd8be12d02e8c692d896b6
okabe783 <134473737+okabe783@users.noreply.github.com>
2024-05-21 16:48:59 +0900

    [Add CinemaChine]playerを追従するカメラとカメラを振動させるようにした

---
+++ Assets/Okabe/Camera/Script/CameraShake.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using Cinemachine;
+
+public class CameraShake : MonoBehaviour
+{
+    private CinemachineVirtualCamera _cinemaChine;
+    private float _shakeTimer;
+
+    private void Awake()
+    {
+        _cinemaChine = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    private void Update()
+    {
+        if (_shakeTimer > 0)
+        {
+            _shakeTimer -= Time.deltaTime;
+            if (_shakeTimer <= 0f)
+            {
+                var cinemaChineBasicMultiChannelPerlin =
+                    _cinemaChine.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+
+                cinemaChineBasicMultiChannelPerlin.m_AmplitudeGain = 0f;
+            }
+        }
+    }
+
+    public void Shake(float timer, float intensity)
+    {
+        var cinemaChineBasicMultiChannelPerlin =
+            _cinemaChine.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+
+        cinemaChineBasicMultiChannelPerlin.m_AmplitudeGain = intensity;
+        _shakeTimer = timer;
+    }
+}
