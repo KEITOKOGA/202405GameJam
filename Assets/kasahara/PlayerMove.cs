3f991e87012601c800f8c08f47a9b90a373f1926
KasaharaSota <souta.programer1201@gmail.com>
2024-05-21 11:47:22 +0900

    プレイヤーの移動のコード

---
+++ Assets/kasahara/PlayerMove.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace playerMove
+{
+    public class Player : MonoBehaviour
+    {
+        public float speed;
+
+        private void Update()
+        {
+            Vector2 _move = Vector2.zero;
+            if (Input.GetKey(KeyCode.A))
+            {
+                _move.x = -1;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                _move.x = 1;
+            }
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                _move.y = 1;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                _move.y = -1;
+            }
+
+            _move.Normalize();
+
+            GetComponent<Rigidbody2D>().velocity = speed * _move;
+        }
+    }
+}
