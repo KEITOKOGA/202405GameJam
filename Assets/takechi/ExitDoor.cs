1e4e798c8dd47bdd3cf4df0d395550e5933ff19d
KasaharaSota <souta.programer1201@gmail.com>
2024-05-22 11:11:26 +0900

    脱出の処理

--- Assets/takechi/ExitDoor.cs
+++ Assets/takechi/ExitDoor.cs
@@ -8,6 +8,10 @@ namespace takechi
         [SerializeField, Header("ゲームクリアシーンの名前")]
         private string _gameClearSceneName;
         private GameManager gameManager;
+        private void Awake()
+        {
+            gameManager = FindAnyObjectByType<GameManager>();
+        }
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.CompareTag("Player") && Player_Item.EntranceKeyCount > 0)
